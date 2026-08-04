ff8438be892dc5b6057d16e0714c18e93c11b2d6
niveltecnologians <nivel.tecnologians@gmail.com>
2026-08-04 13:23:01 -0500

    Add files via upload

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -57,7 +57,7 @@ export default async function ProjectPage({ params }: { params: { id: string } }
           </p>
         </div>
         {canManage && (
-          <div className="flex gap-2">
+          <div className="flex flex-wrap gap-2">
             <EditProjectForm
               projectId={project.id}
               initialName={project.name}
